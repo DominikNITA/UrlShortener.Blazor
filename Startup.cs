c85cc5112737f14cdf7a1e517a66e2eae5470d3a
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2020-10-25 00:52:28 +0200

    Added Npgsql to connect to Heroku Database

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,7 +40,7 @@ namespace UrlShortener
             }
             else
             {
-                services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(Environment.GetEnvironmentVariable("DATABASE_URL")));
+                services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(GetConnectionStringToHerokuDatabase()));
             }
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
@@ -87,6 +88,24 @@ namespace UrlShortener
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
+        }
+
+        public string GetConnectionStringToHerokuDatabase()
+        {
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/')
+            };
+
+            return builder.ToString();
         }
     }
 }
